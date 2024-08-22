e0e39eb210876e7ab1b98714d4eee5641822f073
dreixcode <dreixcode@gmail.com>
2024-08-21 22:41:38 -0700

    Route and Login Register - Part 1

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,9 @@
+import { useState } from "react";
+import Router from "./router/Router";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello, this is dreiXCode. Welcome to my eCommerce project!
-    </h1>
-    </div>
-  );
+  const [allRoutes, setAllRoutes] = useState([])
+  return <Router allRoutes={allRoutes}/>
 }
 
 export default App;
