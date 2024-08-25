7326b9e882f774c87212ef32ed1e5babc2e4ccb2
dreixcode <dreixcode@gmail.com>
2024-08-24 22:06:43 -0700

    Login and Register Page State - Part 1

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom'
 import { FaFacebook, FaGoogle } from "react-icons/fa";
 
 const Register = () => {
+
+const [state, setState] = useState({
+    name:"",
+    email:"",
+    password: ""
+})
+
+const inputHandle = (e) => {
+    setState({
+        ...state,
+        [e.target.name] : e.target.value
+    })
+}
+
+const submit = (e) => {
+    e.preventDefault()
+    console.log(state)
+}
+
     return (
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
             <div className='w-[350px] text-[#ffffff] p-2'>
@@ -10,27 +29,27 @@ const Register = () => {
                     <h2 className='text-xl mb-3 font-bold'>Welcome to dreiXShop</h2>
                     <p className='text-sm mb-3 font-medium'>Please register your account</p>
 
-                    <form>
+                    <form onSubmit={submit} >
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor="name">Name</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='name' placeholder='name' id='name' required/>
+                            <input onChange={inputHandle} value={state.name} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='name' placeholder='name' id='name' required/>
 
                         </div>
 
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor="email">Email</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='email' placeholder='email' id='email' required/>
+                            <input onChange={inputHandle} value={state.email} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='email' placeholder='email' id='email' required/>
 
                         </div>
 
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor="password">Password</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='password' placeholder='password' id='name' required/>
+                            <input onChange={inputHandle} value={state.password} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='password' placeholder='password' id='password' required/>
 
                         </div>
 
                         <div className='flexn items-center w-full gap-3 mb-3'>
-                            <input className='w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300 focus:ring-blue-500' type="checkbox" name="checkbox" id="checkbox"/>
+                            <input onChange={inputHandle} value={state.password} className='w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300 focus:ring-blue-500' type="checkbox" name="checkbox" id="checkbox"/>
                             <label htmlFor="checkbos"> Terms and Condition</label>
 
                         </div>
@@ -39,7 +58,6 @@ const Register = () => {
 
                         <div className='flex items-center mb-3 gap-3 justify-center'>
                             <p>Already have an acount? <Link className='font-bold' to="/login">Sign In</Link></p>
-
                         </div>
 
                         <div className='w-full flex justify-center items-center mb-3'>
