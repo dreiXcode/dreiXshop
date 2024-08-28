efd71851ab2ab567eede4faa76d5aff6c187e5e7
dreixcode <dreixcode@gmail.com>
2024-08-27 21:46:16 -0700

    Backend Setup - Part 3

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -4,11 +4,10 @@ import api from "../../api/api";
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
     async(info) => {
-        // console.log(info)
+        console.log(info)
         try {
             const {data} = await api.post('/admin-login',info, {withCredentials: true})
-            console.log(data)
-            
+            console.log(data)            
         } catch (error) {
             
         }
