061632a15af6291c824914c0596e12022476ceed
dreixcode <dreixcode@gmail.com>
2024-08-30 21:26:25 -0600

    Error and Notification Message - Part 1

--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { admin_login } from '../../store/Reducers/authReducer';
+import { PropagateLoader } from 'react-spinners'; 
 
 
 const AdminLogin = () => {
 
     const dispatch = useDispatch()
+    const {loader} = useSelector(state => state.auth)
     
     const [state, setState] = useState({
         email:"",
@@ -23,6 +25,14 @@ const AdminLogin = () => {
         e.preventDefault()
         dispatch(admin_login(state))
         //console.log(state)
+    }
+
+    const overrideStyle = {
+        display : 'flex',
+        margin : '0 auto',
+        height : '24px',
+        justifyContent : 'center',
+        alignItem : 'center'
     }
 
     return (
@@ -50,7 +60,13 @@ const AdminLogin = () => {
 
                         </div>
 
-                        <button className='bg-slate-800 w-full hover:shadow-blue-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign In</button>
+                        <button disabled={loader ? true : false} className='bg-slate-800 w-full hover:shadow-blue-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
+                        {
+                            loader ? <PropagateLoader color = '#fff' cssOverride={overrideStyle}/> : 'Sign In'
+                        }
+                        
+                        
+                        </button>
                     </form>                    
                 </div>
             </div>            
