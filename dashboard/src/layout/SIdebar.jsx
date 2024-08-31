84d4d9112b7a1a11d5e373b08087300bc2a19a9c
dreixcode <dreixcode@gmail.com>
2024-08-31 12:14:41 -0600

    Page Navigation - Part 3

--- dashboard/src/layout/SIdebar.jsx
+++ dashboard/src/layout/SIdebar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { getNav } from '../navigation';
+import { BiLogOutCircle } from "react-icons/bi";
 
 const Sidebar = () => {
 
@@ -33,8 +34,14 @@ const Sidebar = () => {
                                 </Link>
                             </li>)
                         }
-                    </ul>
 
+                        <li>
+                            <button className='[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1'>
+                                <span><BiLogOutCircle /></span>
+                                <span>Logout</span>
+                            </button>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
--- dashboard/src/navigation/allNav.js
+++ dashboard/src/navigation/allNav.js
@@ -1,4 +1,9 @@
-import { AiFillDashboard } from "react-icons/ai";
+import { AiFillDashboard, AiOutlineShoppingCart } from "react-icons/ai";
+import { BiCategory } from "react-icons/bi";
+import { FaUsers, FaUserTimes } from "react-icons/fa";
+import { MdPayment } from "react-icons/md";
+import { FaCodePullRequest } from "react-icons/fa6";
+import { IoChatbubblesSharp } from "react-icons/io5";
 
 export const allNav = [
     {
@@ -7,5 +12,55 @@ export const allNav = [
         icon : <AiFillDashboard />,
         role : 'admin',
         path : '/admin/dashboard'
+    },
+    {
+        id : 2,
+        title : 'Orders',
+        icon : <AiOutlineShoppingCart />,
+        role : 'admin',
+        path : '/admin/dashboard/orders'
+    },
+    {
+        id : 3,
+        title : 'Category',
+        icon : <BiCategory />,
+        role : 'admin',
+        path : '/admin/dashboard/category'
+    },
+    {
+        id : 4,
+        title : 'Sellers',
+        icon : <FaUsers />,
+        role : 'admin',
+        path : '/admin/dashboard/sellers'
+    },
+    {
+        id : 5,
+        title : 'Payment Request',
+        icon : <MdPayment />,
+        role : 'admin',
+        path : '/admin/dashboard/payment-request'
+    },
+    {
+        id : 6,
+        title : 'Deactivate Sellers',
+        icon : <FaUserTimes />,
+        role : 'admin',
+        path : '/admin/dashboard/deactive-sellers'
+    },
+    {
+        id : 7,
+        title : 'Seller Request',
+        icon : <FaCodePullRequest />,
+        role : 'admin',
+        path : '/admin/dashboard/sellers-request'
+    },
+    {
+        id : 8,
+        title : 'Live Chat',
+        icon : <IoChatbubblesSharp />,
+        role : 'admin',
+        path : '/admin/dashboard/chat-seller'
     }
+    
 ]
