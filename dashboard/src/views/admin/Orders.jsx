718c63a8246639a906259540914e27db17f68fc7
dreixcode <dreixcode@gmail.com>
2024-08-31 17:49:48 -0600

    Admin Order Design - Part 2

--- dashboard/src/views/admin/Orders.jsx
+++ dashboard/src/views/admin/Orders.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { LuArrowDownSquare } from "react-icons/lu";
+import { Link } from 'react-router-dom';
 
 const Orders = () => {
     const [currenPage, setCurrentPage] = useState(1)
@@ -15,6 +17,35 @@ const Orders = () => {
                         <option value="20">20</option>
                     </select>
                     <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" placeholder='search' />
+                </div>
+
+                <div className='relative mt-5 overflow-x-auto'>
+                    <div className='w-full text-sm text-left [#d0d2d6]'>
+                        
+                        <div className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
+                            <div className='flex justify-between items-center'>
+                                <div className='py-3 w-[25%] font-bold'>Order Id</div>
+                                <div className='py-3 w-[13%] font-bold'>Price</div>
+                                <div className='py-3 w-[18%] font-bold'>Payment Status</div>
+                                <div className='py-3 w-[18%] font-bold'>Order Status</div>
+                                <div className='py-3 w-[18%] font-bold'>Action</div>
+                                <div className='py-3 w-[8%] font-bold'><LuArrowDownSquare /></div>                           
+                            </div>
+                        </div>
+
+                        <div className='text-[#d0d2d6]'>
+                            <div className='flex justify-between items-start border-b border-slate-700'>
+                                <div className='py-3 w-[25%] font-medium whitespace-nowrap'>55445</div>
+                                <div className='py-3 w-[13%] font-medium'>$4534</div>
+                                <div className='py-3 w-[18%] font-medium'>Pending</div>
+                                <div className='py-3 w-[18%] font-medium'>Pending</div>
+                                <div className='py-3 w-[18%] font-medium'>
+                                    <Link>View</Link>
+                                </div>
+                                <div className='py-3 w-[8%] font-medium'><LuArrowDownSquare /></div>                           
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>            
         </div>
