4f97e47a8efecb96eba9d8e579da175d995d3133
dreixcode <dreixcode@gmail.com>
2024-08-31 19:52:53 -0600

    Create Pagination - Part 3

--- dashboard/src/views/Pagination.jsx
+++ dashboard/src/views/Pagination.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
+import { MdOutlineKeyboardDoubleArrowLeft, MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 const Pagination = ({pageNumber, setPageNumber, totalItem, parPage, showItem}) => {
     let totalPage = Math.ceil(totalItem / parPage)
@@ -24,21 +24,25 @@ const Pagination = ({pageNumber, setPageNumber, totalItem, parPage, showItem}) =
                 <li onClick={()=> setPageNumber(i)} className={`${pageNumber === i ? 'bg-indigo-300 shadow-lg shadow-indigo-300/50 text-white' : 'bg-slate-600 hover:bg-indigo-400 shadow-lg hover:shadow-indigo-500/50 hover:text-white text-[#d0d2d6]'}  w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer`}>
                     {i}
                 </li>
-            )
-            
+            )            
         }
-
+        
         return btns
 
     }
     return (
         <ul className='flex gap-3'>
             {
-                pageNumber > 1 && <li className='w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-300 text-[#000000]'>
+                pageNumber > 1 && <li onClick={() => setPageNumber(pageNumber-1) } className='w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-300 text-[#000000] cursor-pointer'>
                     <MdOutlineKeyboardDoubleArrowLeft />
                 </li>
             }{
                 createBtn()
+            }
+            {
+                pageNumber < totalPage && <li onClick={() => setPageNumber(pageNumber+1) } className='w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-300 text-[#000000] cursor-pointer'>
+                    <MdOutlineKeyboardDoubleArrowRight />
+                </li>
             }
         </ul>
     )
