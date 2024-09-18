dc54e0c597237139c311f526ce1939eb6b7c65c3
dreixcode <dreixcode@gmail.com>
2024-09-17 23:11:25 -0600

    Seller Request Show Add Admin - Part 1

---
+++ dashboard/src/store/Reducers/sellerReducer.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const get_seller_request = createAsyncThunk(
+    'seller/get_seller_request',
+    async({parPage, page, searchValue}, {rejectWithValue, fulfillWithValue}) => {
+        
+        try {
+            const {data} = await api.get(`/request-seller-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`, {withCredentials: true})
+            //console.log(data)
+            return fulfillWithValue(data)            
+        } catch (error) {
+            //console.log(error.response.data)
+            return rejectWithValue(error.response.data); 
+
+        }
+    }
+)
+
+export const sellerReducer =  createSlice ({
+    name: 'seller',
+    initialState: {
+        successMessage: '',
+        errorMessage: '',
+        loader: false,
+        userInfo: '',
+        sellers: [],
+        totalSeller: 0
+    },
+    reducers : {
+        messageClear : (state, _) => {
+            state.errorMessage = "";
+            state.successMessage = "";
+        }
+
+    },
+    extraReducers : (builder) => {
+        // builder
+        // .addCase(get_category.fulfilled, (state, {payload}) => {
+        //     state.totalCategory = payload.totalCategory;
+        //     state.categories = payload.categories;
+        // })
+    }
+})
+
+export const {messageClear} = sellerReducer.actions;
+export default sellerReducer.reducer;
