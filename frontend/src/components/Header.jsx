297a1cf98f215c972fc480ef22199667deccb04c
dreixcode <dreixcode@gmail.com>
2024-09-19 00:19:26 -0600

    Project Setup, Folder, Route Manage Admin Frontend- Part 2

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { MdEmail } from "react-icons/md";
+import { IoMdPhonePortrait } from "react-icons/io";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { TiArrowSortedDown } from "react-icons/ti";
 
 const Header = () => {
     return (
-        <div>
-            header
+        <div className='w-full bg-white'>
+            <div className='header-top bg-[#caddff] md-lg:hidden'>
+                <div className='w-[85%] lg:w-[90%] mx-auto'>
+                    <div className='flex w-full justify-between items-center h-[50px] text-slate-500'>
+
+                        <ul className='flex justify-start items-center gap-8 font-semibold text-black'>
+
+                            <li className='flex relative justify-center items-center gap-2 text-sm after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px]'>
+                                <span><MdEmail /></span>
+                                <span>support@gmail.com</span>
+                            </li>
+
+                            <li className='flex relative justify-center items-center gap-2 text-sm'>
+                                <span><IoMdPhonePortrait /></span>
+                                <span>+(123) 456 7890</span>
+                            </li>
+
+                        </ul>
+
+                        <div>
+                            <div className='flex justify-center items-center gap-10'>
+                                <div className='flex justify-center items-center gap-4 text-black'>
+                                    <a href="#"><FaFacebookF /></a>
+                                    <a href="#"><FaTwitter /></a>
+                                    <a href="#"><FaLinkedin /></a>
+                                    <a href="#"><FaGithub /></a>
+                                </div>
+
+                                <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
+                                    <img src="http://localhost:3000/images/Language.png" alt="" />
+                                    <span><TiArrowSortedDown /></span>
+                                    <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
+                                        <li>Hindi</li>
+                                        <li>English</li>
+                                    </ul>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            
         </div>
     );
 };
