f7fc82f7eae6166e64e11cf1c274c83e52d2ffd6
dreixcode <dreixcode@gmail.com>
2024-09-19 20:35:27 -0600

    Header Design Tailwind CSS React Frontend - Part 5

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,6 +8,7 @@ import { FaGithub } from "react-icons/fa";
 import { TiArrowSortedDown } from "react-icons/ti";
 import { Link, useLocation } from 'react-router-dom';
 import { AiFillHeart } from 'react-icons/ai';
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Header = () => {
 
@@ -146,7 +147,7 @@ const Header = () => {
             <div className='hidden md-lg:block'>
                 <div onClick={()=> setShowSidebar(true)} className={`fixed duration-200 transition-all ${showSidebar ? 'invisible' : 'visible'} hidden md-lg:block w-screen h-screen bg-[rgba(0,0,0,0.5)] top-0 left-0 z-20`}>
                 </div>
-                
+
                 <div className={`w-[300px] z-[999] transition-all duration-200 fixed ${showSidebar ? '-left-[300px]' : 'left-0 top-0'} overflow-y-auto bg-white h-screen py-6 px-8 `}>
 
                     <div className='flex justify-start flex-col gap-6'>
@@ -198,6 +199,31 @@ const Header = () => {
                                 <Link className={`py-2 block ${pathname === '/contact' ? 'text-[#059473]' : 'text-slate-600' }`}>Contact Us</Link>
                             </li> 
                         </ul>
+                        
+                        <div className='flex justify-start items-center gap-4 text-black'>
+                            <a href="#"><FaFacebookF /></a>
+                            <a href="#"><FaTwitter /></a>
+                            <a href="#"><FaLinkedin /></a>
+                            <a href="#"><FaGithub /></a>
+                        </div>
+
+                        <div className='w-full flex justify-end md-lg:justify-start gap-3 items-center'>
+                            <div className='w-[48px] h-[48px] rounded-full flex bg-[#f5f5f5] justify-center items-center'>
+                                <span><FaPhoneAlt /></span>
+                            </div>
+
+                            <div className='flex justify-end flex-col gap-1'>
+                                <h2 className='text-sm font-medium text-slate-700'>+12342424242</h2>
+                                <span className='text-sm'>Support 24/7</span>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-col justify-start items-start gap-3 text-[#1c1c1c]'>
+                            <li className='flex justify-start items-center gap-2 text-sm'>
+                                <span><MdEmail /></span>
+                                <span>support@gmail.com</span>
+                            </li>
+                        </div>
 
                     </div>
                     
