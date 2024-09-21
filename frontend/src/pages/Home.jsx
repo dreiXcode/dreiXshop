a25311419826362746bc5e01293e8354dd742a86
dreixcode <dreixcode@gmail.com>
2024-09-20 21:49:22 -0600

    Setup Footer Area Design Frontend - Part 1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Banner from '../components/Banner';
 import Categories from '../components/Categories';
 import FeatureProducts from '../components/products/FeatureProducts';
 import Products from '../components/products/Products';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
@@ -35,6 +36,11 @@ const Home = () => {
 
                     </div>
                 </div>
+            </div>
+
+            <div>
+                <Footer/>
+
             </div>
 
         </div>
