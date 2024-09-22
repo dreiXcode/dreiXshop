fa738b66d30eb5c2ec456211c091e08b745f65d9
dreixcode <dreixcode@gmail.com>
2024-09-21 23:32:57 -0600

    Card Page Design Setup Frontend - Part 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Shops from './pages/Shops';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>      
-        <Route path='/shops' element={<Shops/>}/>        
+        <Route path='/shops' element={<Shops/>}/>   
+        <Route path='/cart' element={<Cart/>}/>        
       </Routes>    
     </BrowserRouter>
   );
