d34d9c5b79b199c13dce9f5cf724496800f1e12f
dreixcode <dreixcode@gmail.com>
2024-09-26 22:43:15 -0600

    Product Details Page Design Frontend - Part 5

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -7,6 +7,10 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css'
 import Ratings from '../components/Rating';
 import { FaHeart } from 'react-icons/fa';
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Details = () => {
 
@@ -168,10 +172,56 @@ const Details = () => {
                                     </div>
                                 </div>
                             </div>
+
+                            <div className='flex py-5 gap-5'>
+                                <div className='w-[150px] text-black font-bold text-xl flex flex-col gap-5'>
+                                    <span>Availability</span>
+                                    <span>Share</span>
+                                </div>
+                                <div className='flex flex-col gap-5'>
+                                    <span className={`text-${stock ? 'green' : 'red'}-500`}>
+                                        {stock ? `In Stock (${stock})` : 'Out of Stock'}
+                                    </span>
+
+                                    <ul className='flex justify-start items-center gap-3'>
+
+                                        <li>
+                                            <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-indigo-500 rounded-full text-white' href='#'><FaFacebookF/></a>
+                                        </li>
+
+                                        <li>
+                                            <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-cyan-500 rounded-full text-white' href='#'><FaTwitter/></a>
+                                        </li>
+                                        
+                                        <li>
+                                            <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-purple-500 rounded-full text-white' href='#'><FaLinkedin/></a>
+                                        </li>
+                                        
+                                        <li>
+                                            <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-blue-500 rounded-full text-white' href='#'><FaGithub/></a>
+                                        </li>                                        
+
+                                    </ul>
+
+                                </div>
+
+                            </div>
                             
+                            <div className='flex gap-3'>
+                                {
+                                    stock ? <button className='px-8 py-3 h-[50px] cursor-pointer hover:shadow-lg hover:shadow-green-500/40 bg-[#247462] text-white'>
+                                        Buy Now
+                                    </button> : ''
+                                }
+                                <Link t0='#' className='px-8 py-3 h-[50px] cursor-pointer hover:shadow-lg hover:shadow-red-500/40 bg-red-500 text-white'>
+                                    Chat Seller
+                                </Link>
+                            </div>
+
                         </div>
 
                     </div>
+                    
 
                 </div>
 
