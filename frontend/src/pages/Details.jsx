f1e5052c07c2ddf940e799e3c77476c58a4c862f
dreixcode <dreixcode@gmail.com>
2024-10-01 14:27:38 -0600

    Product Details Page Design Frontend - Part 9

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -219,7 +219,7 @@ const Details = () => {
                                         Buy Now
                                     </button> : ''
                                 }
-                                <Link t0='#' className='px-8 py-3 h-[50px] cursor-pointer hover:shadow-lg hover:shadow-red-500/40 bg-red-500 text-white'>
+                                <Link to='#' className='px-8 py-3 h-[50px] cursor-pointer hover:shadow-lg hover:shadow-red-500/40 bg-red-500 text-white'>
                                     Chat Seller
                                 </Link>
                             </div>
@@ -311,7 +311,7 @@ const Details = () => {
                             loop={true}
                             pagination={{
                                 clickable : true,
-                                el : '.custome_bullet'
+                                el : '.custom_bullet'
                             }}
                             modules={[Pagination]}
                             className='mySwiper'
@@ -323,13 +323,24 @@ const Details = () => {
                                         <Link className='block'>
                                             <div className='relative h-[270px]'>
                                                 <div className='w-full h-full'>
-                                                    <img src={`http://localhost:3000/images/products/${p}.webp`} alt="" />
+                                                    <img className='w-full h-full' src={`http://localhost:3000/images/products/${p}.webp`} alt="" />
                                                     <div className='absolute h-full w-full top-0 left-0 bg-[#000] opacity-25 hover:opacity-50 transition-all duration-500'>
                                                     </div>
                                                 </div>
-                                                
-                                                    discount !== 0 && <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>{discount}%
+                                                {                                                    
+                                                discount !== 0 && <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>{discount}%
+                                                </div>
+                                                }
+                                            </div>
+
+                                            <div className='p-4 flex flex-col gap-1 '>
+                                                <h2 className='text-slate-600 text-lg font-bold'>Product Name</h2>
+                                                <div className='flex justify-start items-center gap-3'>
+                                                    <h2 className='text-lg font-bold text-slate-600'>$343</h2>
+                                                    <div className='flex items-center gap-2'>
+                                                        <Ratings ratings={4.5}/>
                                                     </div>
+                                                </div>
                                             </div>
                                         </Link>
                                     </SwiperSlide>
@@ -337,6 +348,13 @@ const Details = () => {
                                 })
                             }
                         </Swiper>
+                    </div>
+
+                    <div className='w-full flex justify-center items-center py-8'>
+                        <div className='custom_bullet justify-center gap-3 !w-auto'>
+
+                        </div>
+                         
                     </div>
                 </div>
             </section>
