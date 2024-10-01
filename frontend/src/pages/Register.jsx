068319f3b734acaee2699b34fb16affa8af280f5
dreixcode <dreixcode@gmail.com>
2024-10-01 17:42:32 -0600

    Customer Login Register Page Design Frontend - Part 3

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { FaFacebook, FaGoogle, FaGooglePlus } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: ''
+    }) 
+
+    const inputHandle = (e) => {
+        setState ({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const register = (e) => {
+        e.preventDefault()
+        console.log(state)
+    }
+
+
     return (
         <div>
             <Header/>
                 <div className='bg-slate-200 mt-4'>
                     <div className='w-full justify-center items-center p-10'>
-                        <div className='grid grid-cols-2 w-[60%] mx-auto bg-white rounded-md'>
+                        <div className='grid grid-cols-2 w-[50%] mx-auto bg-white rounded-md'>
                             
                             <div className='px-8 py-8'>
                                 <h2 className='text-center w-full text-xl text-slate-600 font-bold'>Register</h2>                           
 
                                 <div>
-                                    <form className='text-slate-600'>
+                                    <form onSubmit={register} className='text-slate-600'>
                                         <div className='flex flex-col gap-1 mb-2'>
                                             <label htmlFor="name">Name</label>
-                                            <input className='w-full px-3 py-2 border border-slate-200 outline-none focus:border-green-500 rounded-md' type="text" name="name" id="name" placeholder="Name" required/>
+                                            <input onChange={inputHandle} value={state.name} className='w-full px-3 py-2 border border-slate-200 outline-none focus:border-green-500 rounded-md' type="text" name="name" id="name" placeholder="Name" required/>
                                         </div>
                                         <div className='flex flex-col gap-1 mb-2'>
                                             <label htmlFor="email">Email</label>
-                                            <input className='w-full px-3 py-2 border border-slate-200 outline-none focus:border-green-500 rounded-md' type="email" name="email" id="email" placeholder="Email" required/>
+                                            <input onChange={inputHandle} value={state.email} className='w-full px-3 py-2 border border-slate-200 outline-none focus:border-green-500 rounded-md' type="email" name="email" id="email" placeholder="Email" required/>
                                         </div>
                                         <div className='flex flex-col gap-1 mb-2'>
                                             <label htmlFor="password">Password</label>
-                                            <input className='w-full px-3 py-2 border border-slate-200 outline-none focus:border-green-500 rounded-md' type="password" name="password" id="password" placeholder="Password" required/>
+                                            <input onChange={inputHandle} value={state.password} className='w-full px-3 py-2 border border-slate-200 outline-none focus:border-green-500 rounded-md' type="password" name="password" id="password" placeholder="Password" required/>
                                         </div>
 
                                         <button className='px-8 w-full py-2 bg-[#059473] shadow-lg hover:shadow-green-500/40 text-white rounded-md'>Register</button>                                    
