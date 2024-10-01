920f382dd6c185c03a729110d879f5194c58922a
dreixcode <dreixcode@gmail.com>
2024-10-01 16:53:32 -0600

    Customer Login Register Page Design Frontend - Part 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,17 @@ import Shops from './pages/Shops';
 import Cart from './pages/Cart';
 import Shipping from './pages/Shipping';
 import Details from './pages/Details';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>      
-        <Route path='/shops' element={<Shops/>}/>   
+        <Route path='/shops' element={<Shops/>}/>         
+        <Route path='/login' element={<Login/>}/>         
+        <Route path='/register' element={<Register/>}/>   
         <Route path='/cart' element={<Cart/>}/>        
         <Route path='/shipping' element={<Shipping/>}/>     
         <Route path='/product/details/:slug' element={<Details/>}/>        
