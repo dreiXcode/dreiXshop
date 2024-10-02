0e6037a1e6685fe61d38870c234309828e51b1b1
dreixcode <dreixcode@gmail.com>
2024-10-01 18:30:52 -0600

    Redux Tookit Setup Frontend

---
+++ frontend/src/store/reducers/homeReducer.js
@@ -0,0 +1,16 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const homeReducer = createSlice({
+    name: 'home',
+    initialState:{
+        categories: [],
+    },
+    reducers : {
+    
+    },
+    extraReducers : (builder) => {
+        
+    }
+})
+
+export default homeReducer.reducer;
