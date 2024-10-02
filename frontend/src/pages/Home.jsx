fcd9f1ebb1a46a60886fbfcbfe6fca7bcaef2f58
dreixcode <dreixcode@gmail.com>
2024-10-02 11:55:00 -0600

    Category Fetch and Show Frontend - Part 1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Categories from '../components/Categories';
 import FeatureProducts from '../components/products/FeatureProducts';
 import Products from '../components/products/Products';
 import Footer from '../components/Footer';
+import { useDispatch, useSelector } from 'react-redux';
+import { get_category } from '../store/reducers/homeReducer';
 
 const Home = () => {
+
+    const dispatch = useDispatch()
+    const {categories} = useSelector(state => state.home)
+    useEffect(() => {
+        dispatch(get_category())
+    },[])
+    
     return (
         <div className='w-full'>
 
--- frontend/src/store/reducers/homeReducer.js
+++ frontend/src/store/reducers/homeReducer.js
@@ -1,4 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const get_category = createAsyncThunk(
+    'product/get_category',
+    async(_, { fulfillWithValue }) => {
+        try {
+            const {data} = await api.get('/home/get-categories')
+            console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+) //End Method
 
 export const homeReducer = createSlice({
     name: 'home',
